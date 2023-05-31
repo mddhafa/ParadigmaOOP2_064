3dae12ab2ddc7ac00c020e90614cdb6c8ea07feb
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-05-31 11:15:41 +0700

    membuat class budi dan menambahkan public pekerja dan pelajar di dalamnya

--- ParadigmaOOP.2/ParadigmaOOP.2.cpp
+++ ParadigmaOOP.2/ParadigmaOOP.2.cpp
@@ -27,3 +27,15 @@ public:
 		cout << "pelajar dibuat\n" << endl;
 	}
 };
+
+class budi : public pekerja, public pelajar {
+public: 
+	
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur)
+	{
+		cout << "Budi dibuat\n" << endl;
+	}
+};
+
