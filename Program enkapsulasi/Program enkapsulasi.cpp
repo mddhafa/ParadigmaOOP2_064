fed123cb6c9ac0d850d71685fe6178138fb8f2a1
mddhafa <mdmuhammaddhafa1234@gmail.com>
2023-05-31 16:43:36 +0700

    menambahkan fungsi utama

--- Program enkapsulasi/Program enkapsulasi.cpp
+++ Program enkapsulasi/Program enkapsulasi.cpp
@@ -12,3 +12,19 @@ public:
 		return saklarNo[i];
 	}
 };
+
+int main() {
+	remoteLampu lampuRumah;
+
+	lampuRumah.serSaklarNo(0, "Lampu Teras Rumah");
+	lampuRumah.serSaklarNo(1, "Lampu Ruang Tamu");
+	lampuRumah.serSaklarNo(2, "Lampu kamar tidur ");
+	lampuRumah.serSaklarNo(3, "Lampu Dapur");
+
+	cout << lampuRumah.getSaklarNo(0) << endl;
+	cout << lampuRumah.getSaklarNo(1) << endl;
+	cout << lampuRumah.getSaklarNo(2) << endl;
+	cout << lampuRumah.getSaklarNo(3) << endl;
+
+	return 0;
+}
